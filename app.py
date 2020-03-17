98eaf24ca671493c490362cb650ad95bcf2ec993
Gabriel Loye <gabrielloye@hotmail.com>
2020-03-17 17:32:29 +0800

    added connection to sql

--- app.py
+++ app.py
@@ -7,34 +7,64 @@ import datetime
 import math
 import pickle
 from sklearn.ensemble import RandomForestRegressor
+import mysql.connector
 
 filename = 'rf_model.sav'
 model = pickle.load(open(filename, 'rb'))
+
+db = mysql.connector.connect(
+    host='localhost',
+    user='gabriel',
+    password='password',
+    database='hfg_db'
+)
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/all', methods=["GET"])
 def all():
-    return jsonify({
-        0: {
-            "name": "Milk",
-            "image": "image_url",
-            "base_price": 5.6,
-            "min_price": 2.3
-        },
-        1: {
-            "name": "Halibut",
-            "image": "image_url",
-            "base_price": 12,
-            "min_price": 6
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM item")
+    return_obj = {}
+    for row in cursor.fetchall():
+        return_obj[row[0]] = {
+            "name": row[1],
+            "image": row[2],
+            "info": row[3],
+            "base_price": row[4],
+            "min_price": row[4]*0.6
         }
-    })
+    return jsonify(return_obj)
 
 @app.route('/item', methods=["GET"])
 def single():
     if 'id' in request.args:
         idx = request.args['id']
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM item WHERE id={}".format(idx))
+        item = cursor.fetchall()[0]
+        return_obj = {
+            "id": idx,
+            "name": item[1],
+            "image": item[2],
+            "info": item[3],
+            "base_price": item[4],
+            "discount": []
+        }
+        price = item[4]
+        wastage = item[5]
+        cursor.execute("SELECT * FROM batch WHERE itemid={}".format(idx))
+        for row in cursor.fetchall():
+            print(row)
+            quantity = row[2]
+            expiry = row[3]
+            days_expiry = (expiry - datetime.datetime.now()).days + 1
+            return_obj['discount'].append({
+                "expiry": expiry,
+                "price": model.predict(np.array([[price, days_expiry, quantity, wastage]])).item()
+            })
+        return jsonify(return_obj)
         return jsonify({
             "id": idx,
             "name": "Halibut",
